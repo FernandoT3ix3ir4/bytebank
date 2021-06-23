3daf7723ff04f528693b2adca7921b9c2946c498
Fernando Teixeira <e.fernandots@gmail.com>
2021-06-22 21:23:15 -0300

    First commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
+})
+export class NovaTransferenciaComponent implements OnInit {
+
+  valor: number;
+  destino: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  transferir() {
+    console.log('Solicitado nova transferência.');
+    console.log('Valor:', this.valor);
+    console.log('Destino:', this.destino);
+  }
+}
