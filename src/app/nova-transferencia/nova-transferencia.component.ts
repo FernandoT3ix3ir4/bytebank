44fa7baf5f589876cf1a81754f29fe52064c13de
Fernando Teixeira <e.fernandots@gmail.com>
2021-06-23 00:29:43 -0300

    conclusao do curso

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { TransferenciaService } from 'src/services/transferencia.service';
 import { Transferencia } from '../models/transferencia.model';
 
@@ -11,20 +12,17 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
-  constructor(private transferenciaService: TransferenciaService) { }
+  constructor(private transferenciaService: TransferenciaService, private router: Router) { }
 
   transferir() {
     const transferencia: Transferencia = { valor: this.valor, destino: this.destino };
     this.transferenciaService.adicionar(transferencia)
       .subscribe(resultado => {
         console.log(resultado);
-        this.limparCampos();
+        this.router.navigateByUrl('extrato');
       }, (error) => console.log(error)
       );
   }
 
-  limparCampos() {
-    this.valor = undefined;
-    this.destino = undefined;
-  }
+
 }
